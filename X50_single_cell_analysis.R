41ef4fe6fb488a1a2f381cdbe91e5a8d3f4b77cf
Xiyu <pansypeng124@gmail.com>
2022-08-05 15:25:10 -0400

    codes for analysis

---
+++ X50_single_cell_analysis.R
@@ -0,0 +1,179 @@
+### 
+# Author: Xiyu Peng
+# single cell analysis for flow cytometry data
+#
+
+
+library(Seurat)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+library(future)
+library(tidyverse)
+
+start_time <- Sys.time()
+Sys.time()
+
+### 4 cores 
+plan("multiprocess", workers = 80)
+plan()
+options(future.globals.maxSize = 100000 * 1024^2)
+
+
+# the working directory
+setwd("/gpfs/mskmind_ess/pengx1/flow/X50_rerun")
+set.seed(42) ## reproducible 
+
+mapping_file <-"/gpfs/mskmind_ess/pengx1/flow/analysis/17-162_file_mapping.csv"
+meta<-read.csv(mapping_file)
+meta$rds_suffix<-paste(meta$SAMPLE_ID,"_intensity_dat_preGated.rds",sep = '')
+
+submeta<-meta 
+count_matrix<-NULL
+downsample<-30000000
+remove_negative<-0
+
+### pool all samples together
+for(i in 1:nrow(submeta)){
+  raw_file<-submeta$rds_suffix[i]
+  data_submat<-NULL
+  tmp<-try(data_submat<-readRDS(raw_file),silent = TRUE)
+  if(!is.null(data_submat)){
+    data_submat<-as.data.frame(data_submat)
+    data_submat<-data_submat[data_submat$cd3_pos == 1,7:34] ##  only keep cd3+ cells and useful columns
+    if(nrow(data_submat) > downsample){
+      data_submat<-data_submat[sample(rownames(data_submat),size = downsample, replace = F),]
+    }
+    if(remove_negative){
+      data_submat[data_submat<0]<-NA
+      data_submat<-na.omit(data_submat)
+    }
+    data_submat$time<-submeta$TP[i]
+    data_submat$patient<-submeta$PATIENT[i]
+    data_submat$sample<-submeta$SAMPLE_ID[i]
+    count_matrix<-rbind(count_matrix,data_submat)
+    rm(data_submat)
+  }
+}
+
+data_mat_cd3pos<-count_matrix
+rm(count_matrix)
+
+setwd("/gpfs/mskmind_ess/pengx1/flow/analysis")
+
+data_real_marks<-as.matrix(data_mat_cd3pos[,-c(10,29:31)])
+dim(data_real_marks)
+colnames(data_real_marks)
+
+
+## relable T cells (needed for creating seurat object)
+new_id<-as.character(1:nrow(data_real_marks))
+rownames(data_real_marks)<-new_id
+
+####---------------------------------------------------------------------------------
+## Analysis
+
+#### create seurat object 
+
+pool_X50 <- CreateSeuratObject(counts = t(data_real_marks), project = "17-162-test")
+pool_X50
+pool_X50[['time']]<-factor(data_mat_cd3pos$time)
+pool_X50[['patient']]<-factor(data_mat_cd3pos$patient)
+pool_X50[['sample']]<-factor(data_mat_cd3pos$sample)
+
+### quality control (remove following samples, due to lack of cells)
+all.samples<-levels(pool_X50$sample)
+rm_samples<-c("17-162-32C","17-162-34B","17-162-07B")
+rm_samples
+all.samples<-all.samples[! all.samples %in% rm_samples]
+pool_X50<-subset(pool_X50,subset = sample %in% all.samples)
+
+## remove object to save memory
+rm(data_real_marks)
+rm(data_mat_cd3pos)
+
+## select genes (may think about to remove the step)
+pool_X50 <- FindVariableFeatures(object = pool_X50)
+VariableFeatures(pool_X50)
+
+### scaled data
+all.genes <- rownames(pool_X50)
+pool_X50 <- ScaleData(pool_X50, features = all.genes)
+
+### linear dimension reduction 
+pool_X50 <- RunPCA(pool_X50, features = all.genes,approx=FALSE,npcs = 26)
+DimPlot(pool_X50, reduction = "pca")
+ElbowPlot(pool_X50,ndims = 26)
+
+Sys.time()
+
+## umap for visualization (based on knn graph)
+pool_X50 <- RunUMAP(pool_X50, dims = 1:26,min.dist = 0.1)
+
+Sys.time()
+
+
+### do graph-based clustering based on pca result
+pool_X50 <- FindNeighbors(pool_X50, dims = 1:26, nn.eps = 0.5,k.param = 5)
+
+Sys.time()
+
+### Run clustering under different resolution
+res<-c(0.5, 0.8, 1, 1.2, 1.5, 2, 2.5, 3)
+
+pool_X50 <- FindClusters(pool_X50, resolution = res)
+
+colnames(pool_X50@meta.data)
+
+## choose resolution = 1.5
+Idents(pool_X50)<-"RNA_snn_res.1.5"
+
+### use visualization to check
+#DimPlot(pool_X50,reduction = "umap", label = TRUE)
+#DimPlot(pool_X50,reduction = "umap",split.by = "time")
+#DimPlot(pool_X50,reduction = "umap",split.by = "patient")
+
+### save the seurat object
+saveRDS(pool_X50, file = "seurat_object_17162_rerun_all_dim26.rds")
+
+#proc.time()
+end_time <- Sys.time()
+end_time - start_time
+
+### codes below are for further analysis
+
+### UMAP plot
+DimPlot(subset(pool_X50, idents = 0:19),reduction = "umap", label = TRUE)
+
+## Feature plots
+FeaturePlot(subset(pool_X50, idents = 0:19), features = c("CD4","CD8","FOXP3","CD45RA","CCR7","LAG3","PD1","CTLA4","KI67"), ncol =3, min.cutoff = 0, slot = "scale.data",max.cutoff = 3)  
+FeaturePlot(subset(pool_X50, idents = 0:19), features = c("CD57","EOMES","TIGIT","TBET","GZM-B","CXCR5","GITR","CD38","CD27"), ncol =3, min.cutoff = 0, slot = "scale.data",max.cutoff = 3)
+FeaturePlot(subset(pool_X50, idents = 0:19), features = c("CD28","CD127","ICOS","CD25","CCR4","TIM3","HLADR"), ncol =3, min.cutoff = 0, slot = "scale.data",max.cutoff = 3)
+
+
+### save the marker average expression matrix for heatmap
+genes.reorder<-c("CD4","CD8","FOXP3","CCR7","CD45RA","GITR","CD25","CCR4","CD27","CD28","CD127","TBET","GZM-B","CD57","EOMES","LAG3","CXCR5","HLADR","KI67","TIM3","CD38","ICOS","CTLA4","PD1","TIGIT")
+subset_object<-subset(pool_X50, downsample = 10000)
+all.genes<-rownames(subset_object)
+mat1<-NULL
+for (gene in all.genes){
+  mat1<-rbind(mat1,tapply(subset_object[['RNA']]@scale.data[gene,],Idents(subset_object),mean))
+}
+rownames(mat1)<-all.genes
+save(mat1,file = "average_cluster.Rdata")
+
+
+### save seurat object of given patients
+patients<-c("17-162-27","17-162-05","17-162EXT-09")
+
+for(pt in patients){
+
+    pt_seurat<-subset(pool_X50,subset = patient == pt)   
+    pt_seurat$pool_cluster<-pt_seurat$RNA_snn_res.1.5  ##keep previous clustering result
+    pt_seurat <- RunUMAP(pt_seurat, dims = 1:26,min.dist = 0.3)
+    saveRDS(pt_seurat,file= paste0("/gpfs/mskmind_ess/pengx1/flow/analysis/seurat_pt",pt,"redo.rds"))
+}
+
+
+sessionInfo()
+
---
+++ nparLD_analysis.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(ggpubr)
+library(patchwork)
+library(ggplot2)
+
+merge.data <- read.csv("~/flow_cytometry/lda/merge_data_X50_1.5_new.csv")
+rm_samples<-c("17-162-32C","17-162-34B","17-162-07B")  ## not contains enough cells
+merge.data<-merge.data[!merge.data$sample %in% rm_samples,]
+merge.data %>% select(starts_with("cluster")) %>% colnames()->colnames_data
+merge.data.reformat<-merge.data %>% gather(.,key = "cluster",value = "freq",colnames_data)
+
+library(nparLD)
+merge.data.sub.reformat<-merge.data.reformat %>% filter(pt %in% names(which(colSums(table(merge.data$time,merge.data$pt))==3)))
+plot_clusters<-paste("cluster",0:19,sep = "")
+
+
+### across imtypes
+for (c in plot_clusters){
+  re<-merge.data.sub.reformat %>% filter(cluster == c) %>% nparLD(freq ~ imtype*time, data = .,subject = "pt",description = FALSE)
+  print(c)
+  print(re)
+}
+
+for (c in plot_clusters){
+  re<-merge.data.sub.reformat %>% filter(cluster == c) %>% nparLD(freq ~ res*time, data = .,subject = "pt",description = FALSE)
+  print(c)
+  print(re)
+}
+
+for (c in plot_clusters){
+  re<-merge.data.sub.reformat %>% filter(cluster == c) %>% nparLD(freq ~ tox*time, data = .,subject = "pt",description = FALSE)
+  print(c)
+  print(re)
+}
+
+
+## organize re$ANOVA.test and re$ANOVA.test.mod.Box into tables
+
+result<-list()
+
+for (c in plot_clusters){
+  
+  ### immunotypes
+  re<-merge.data.sub.reformat %>% filter(cluster == c) %>% nparLD(freq ~ imtype*time, data = .,subject = "pt",description = FALSE)
+  imm_array<-c(array(t(re$ANOVA.test)),re$ANOVA.test.mod.Box)
+  imm_header<-outer(rownames(t(re$ANOVA.test)),colnames(t(re$ANOVA.test)),paste, sep = ".")
+  imm_header<-c(imm_header, outer(rownames(t(re$ANOVA.test.mod.Box)),colnames(t(re$ANOVA.test.mod.Box)),paste, sep = "."))
+  imm_header[13]<-"p-value.mod.imtype"
+  
+  ### response
+  re<-merge.data.sub.reformat %>% filter(cluster == c) %>% nparLD(freq ~ res*time, data = .,subject = "pt",description = FALSE)
+  res_array<-c(array(t(re$ANOVA.test)),re$ANOVA.test.mod.Box)
+  res_header<-outer(rownames(t(re$ANOVA.test)),colnames(t(re$ANOVA.test)),paste, sep = ".")
+  res_header<-c(res_header, outer(rownames(t(re$ANOVA.test.mod.Box)),colnames(t(re$ANOVA.test.mod.Box)),paste, sep = "."))
+  res_header[13]<-"p-value.mod.res"
+  
+  ### toxicity
+  re<-merge.data.sub.reformat %>% filter(cluster == c) %>% nparLD(freq ~ tox*time, data = .,subject = "pt",description = FALSE)
+  tox_array<-c(array(t(re$ANOVA.test)),re$ANOVA.test.mod.Box)
+  tox_header<-outer(rownames(t(re$ANOVA.test)),colnames(t(re$ANOVA.test)),paste, sep = ".")
+  tox_header<-c(tox_header, outer(rownames(t(re$ANOVA.test.mod.Box)),colnames(t(re$ANOVA.test.mod.Box)),paste, sep = "."))
+  tox_header[13]<-"p-value.mod.tox"
+  
+  result[[c]]<-c(imm_array,res_array,tox_array)
+  
+}
+
+resultdf<-t(as.data.frame(result))
+colnames(resultdf)<-c(imm_header,res_header,tox_header)
+resultdf<-as.data.frame(resultdf)
+
+## adjust pvalue with selected columns
+select_col<-c("p-value.mod.imtype","p-value.mod.res","p-value.mod.tox","p-value.imtype:time","p-value.res:time","p-value.tox:time")
+adj_pvalues<-resultdf %>% 
+  select(select_col) %>% 
+  mutate(across(select_col,p.adjust,method = "BH"))
+colnames(adj_pvalues)<-paste0("adj.",colnames(adj_pvalues))
+resultdf<-cbind(resultdf,adj_pvalues)
+
+## write the result
+write.csv(resultdf,file = "~/flow_cytometry/lda/single_cluster_stat_analysis.csv")
+
+
+#### make boxplots
+colors<-c("#D55E00", "#009E73", "#0072B2")
+## immunotypes
+plot_clusters<-paste("cluster",0:19,sep = "")
+ps_imtype<-list()
+for (cluster in plot_clusters){
+  ## boxplot with data points connected with lines
+  #p<-ggplot(merge.data,aes(x = time, y = !!ensym(cluster)))+geom_boxplot(aes(color = imtype))+geom_point(aes(color = imtype))+facet_grid(.~imtype)+geom_line(aes(group = pt),color = 'gray')+theme_minimal()
+  p<-ggboxplot(merge.data, x = "time", y = cluster,add = "point",color = "imtype",palette = "jco",facet.by = "imtype")+geom_line(aes(group = pt),color = 'gray')+theme(legend.position = "none")
+  ps_imtype[[cluster]]<-p
+}
+layout1<-"
+ABCD
+EFGH
+IJKL
+MNOP
+"
+wrap_plots(ps_imtype[1:20] ,guides = 'collect')
+ggsave("imtype_clusters.png",width = 16, height = 16,units = "in")
+
+
+
+## response 
+merge.data$res<-factor(merge.data$res)
+levels(merge.data$res)
+levels(merge.data$res)<-c("PD/SD","CR/PR")
+ps_res<-list()
+for (cluster in plot_clusters){
+  ## boxplot with data points connected with lines
+  #p<-ggplot(merge.data,aes(x = time, y = !!ensym(cluster)))+geom_boxplot(aes(color = imtype))+geom_point(aes(color = imtype))+facet_grid(.~imtype)+geom_line(aes(group = pt),color = 'gray')+theme_minimal()
+  p<-ggboxplot(merge.data, x = "time", y = cluster,add = "point",color = "res",palette = "jco",facet.by = "res")+geom_line(aes(group = pt),color = 'gray')+theme(legend.position = "none")
+  ps_res[[cluster]]<-p
+}
+
+wrap_plots(ps_res[1:20] ,guides = 'collect')
+ggsave("res_clusters.png",width = 12, height = 15,units = "in")
+
+
+## tox 
+ps_tox<-list()
+for (cluster in plot_clusters){
+  ## boxplot with data points connected with lines
+  #p<-ggplot(merge.data,aes(x = time, y = !!ensym(cluster)))+geom_boxplot(aes(color = imtype))+geom_point(aes(color = imtype))+facet_grid(.~imtype)+geom_line(aes(group = pt),color = 'gray')+theme_minimal()
+  p<-ggboxplot(merge.data, x = "time", y = cluster,add = "point",color = "tox",palette = "jco",facet.by = "tox")+geom_line(aes(group = pt),color = 'gray')+theme(legend.position = "none")
+  ps_tox[[cluster]]<-p
+}
+
+wrap_plots(ps_tox[1:20] ,guides = 'collect')
+ggsave("tox_clusters.png",width = 12, height = 15,units = "in")
+
+
